6295197c13d75e2b78d60be0839923c02fd6c293
pk13182 <165264165+pk13182@users.noreply.github.com>
2024-12-27 09:24:23 +0000

    1

---
+++ 01base/variables.js
@@ -0,0 +1,15 @@
+const account_id=34567
+var accountEmail="pk@gmail.com"
+let accountPasword="34456%%"
+accountCity="Patna"
+
+
+// account_id=23456  Not Allowed
+accountEmail="rj@gmail.com"
+accountPasword="34456"
+accountCity="Delhi"
+let accountstate;
+console.table([account_id,
+    accountEmail,
+    accountPasword,
+    accountCity,accountstate]);
